42597a5330b52e19c71fdb3ae5fcfe3e9650e878
JULIA ZHOU <jzhouerm@hotmail.com>
2020-05-19 03:55:38 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -5,11 +5,16 @@
 #end
 
 
-def using_until
-  levitation_force = 6
-  until levitation_force == 10
-    puts "Wingardium Leviosa"
-    levitation_force += 1
-  end
-end
+#def using_until
+ # levitation_force = 6
+  #until levitation_force == 10
+   # puts "Wingardium Leviosa"
+    #levitation_force += 1
+  #end
+#end
 
+levitation_force = 6
+until levitation_force == 10
+ puts "Wingardium Leviosa"
+ levitation_force += 1 
+ end
